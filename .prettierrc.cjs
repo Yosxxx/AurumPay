cd546ac96fd372207fd9570eee6e84523a41644b
Yosua <YosuaMontolaluXD@gmail.com>
2025-11-24 17:24:11 +0700

    Added Blade Formatter + Prettier For PHP

---
+++ .prettierrc.cjs
@@ -0,0 +1,12 @@
+export default {
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "all",
+  phpVersion: "8.2",
+  plugins: [
+    "@prettier/plugin-php",
+    "prettier-plugin-blade",
+    "prettier-plugin-tailwindcss",
+  ]
+};
